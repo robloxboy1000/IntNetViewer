25d21aa3f1c7b0b41bc1397b9ea49849335d8668
robloxboy100058 <83027818+robloxboy100058@users.noreply.github.com>
2023-07-20 01:22:11 -0500

    v1.0.1

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.webBrowser1 = new System.Windows.Forms.WebBrowser();
             this.textbox = new System.Windows.Forms.TextBox();
@@ -35,6 +36,12 @@
             this.forward = new System.Windows.Forms.Button();
             this.refresh = new System.Windows.Forms.Button();
             this.go = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.testToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // webBrowser1
@@ -45,7 +52,7 @@
             this.webBrowser1.Location = new System.Drawing.Point(12, 62);
             this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
             this.webBrowser1.Name = "webBrowser1";
-            this.webBrowser1.Size = new System.Drawing.Size(984, 655);
+            this.webBrowser1.Size = new System.Drawing.Size(984, 633);
             this.webBrowser1.TabIndex = 0;
             // 
             // textbox
@@ -53,14 +60,14 @@
             this.textbox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.textbox.Location = new System.Drawing.Point(276, 12);
+            this.textbox.Location = new System.Drawing.Point(270, 33);
             this.textbox.Name = "textbox";
             this.textbox.Size = new System.Drawing.Size(606, 20);
             this.textbox.TabIndex = 1;
             // 
             // back
             // 
-            this.back.Location = new System.Drawing.Point(13, 13);
+            this.back.Location = new System.Drawing.Point(15, 31);
             this.back.Name = "back";
             this.back.Size = new System.Drawing.Size(76, 23);
             this.back.TabIndex = 2;
@@ -70,7 +77,7 @@
             // 
             // forward
             // 
-            this.forward.Location = new System.Drawing.Point(96, 12);
+            this.forward.Location = new System.Drawing.Point(97, 31);
             this.forward.Name = "forward";
             this.forward.Size = new System.Drawing.Size(75, 23);
             this.forward.TabIndex = 3;
@@ -80,7 +87,7 @@
             // 
             // refresh
             // 
-            this.refresh.Location = new System.Drawing.Point(178, 12);
+            this.refresh.Location = new System.Drawing.Point(178, 31);
             this.refresh.Name = "refresh";
             this.refresh.Size = new System.Drawing.Size(75, 23);
             this.refresh.TabIndex = 4;
@@ -91,7 +98,7 @@
             // go
             // 
             this.go.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.go.Location = new System.Drawing.Point(912, 13);
+            this.go.Location = new System.Drawing.Point(899, 31);
             this.go.Name = "go";
             this.go.Size = new System.Drawing.Size(75, 23);
             this.go.TabIndex = 5;
@@ -99,20 +106,63 @@
             this.go.UseVisualStyleBackColor = true;
             this.go.Click += new System.EventHandler(this.go_Click);
             // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.testToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1008, 24);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // testToolStripMenuItem
+            // 
+            this.testToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.testToolStripMenuItem.Name = "testToolStripMenuItem";
+            this.testToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.testToolStripMenuItem.Text = "Help";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.toolStripMenuItem1.Text = "About IntNetViewer";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(896, 701);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(100, 23);
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar1.TabIndex = 7;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 15000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1008, 729);
+            this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.go);
             this.Controls.Add(this.refresh);
             this.Controls.Add(this.forward);
             this.Controls.Add(this.back);
             this.Controls.Add(this.textbox);
             this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.menuStrip1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "IntNetViewer";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -126,6 +176,11 @@
         private System.Windows.Forms.Button forward;
         private System.Windows.Forms.Button refresh;
         private System.Windows.Forms.Button go;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem testToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,22 @@ namespace IntNetViewer
 {
     public partial class Form1 : Form
     {
+        private ProgressBar progressBar;
+
         public Form1()
         {
             InitializeComponent();
+
+            // Create and set up the progress bar
+            progressBar = new ProgressBar();
+            progressBar1.Visible = false;
+            progressBar1.Width = 200;     // Adjust the width according to your layout
+            progressBar1.Location = new Point(10, this.ClientSize.Height - progressBar.Height - 10);
+            this.Controls.Add(progressBar);
+            // Subscribe to the necessary events
+            webBrowser1.Navigating += webBrowser1_Navigating;
+            webBrowser1.Navigated += webBrowser1_Navigated;
+            webBrowser1.ProgressChanged += webBrowser1_ProgressChanged;
         }
 
         private void back_Click(object sender, EventArgs e)
@@ -35,6 +48,46 @@ namespace IntNetViewer
         private void go_Click(object sender, EventArgs e)
         {
             webBrowser1.Navigate(textbox.Text);
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            AboutForm aboutForm = new AboutForm();
+            aboutForm.ShowDialog();
+        }
+        private void webBrowser1_Navigating(object sender, WebBrowserNavigatingEventArgs e)
+        {
+            // Show the progress bar when a new page starts loading
+            progressBar1.Visible = true;
+            progressBar1.Value = 0;
+
+            // Start the Timer to trigger the delay before hiding the progress bar
+            timer1.Start();
+        }
+        private void webBrowser1_ProgressChanged(object sender, WebBrowserProgressChangedEventArgs e)
+        {
+            if (e.MaximumProgress > 0)
+            {
+                // Calculate and set the progress value between 0 and 100
+                int progress = (int)(e.CurrentProgress * 100 / e.MaximumProgress);
+                Console.WriteLine($"Progress: {progress}%");
+                progressBar1.Value = progress;
+            }
+        }
+        private void webBrowser1_Navigated(object sender, WebBrowserNavigatedEventArgs e)
+        {
+            // Hide the progress bar when the page has finished loading
+           
+        
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // Stop the Timer
+            timer1.Stop();
+
+            // Hide the progress bar after the delay
+            progressBar1.Visible = false;
         }
     }
 }
