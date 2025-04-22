cc65a5e5760f20f64a9eee7deb13d39ed70bf3ef
PixlPlaya5 <pixlplaya5@gmail.com>
2025-04-22 14:21:52 -0500

    edit assembly info and resources file

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -81,16 +81,6 @@ namespace IntNetViewer.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
-        /// </summary>
-        internal static System.Drawing.Icon gas {
-            get {
-                object obj = ResourceManager.GetObject("gas", resourceCulture);
-                return ((System.Drawing.Icon)(obj));
-            }
-        }
-        
-        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
         internal static System.Drawing.Bitmap home {
